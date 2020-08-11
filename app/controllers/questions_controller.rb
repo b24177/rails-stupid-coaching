cccf3fd106c34d0872dc4b052bc9650bdd64ba4e
Brant Monaghan <monaghanb@gmail.com>
2020-08-11 21:25:04 +0200

    Even more progress on Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,11 +5,11 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:member]
     if @question == 'I am going to work'
-      puts 'Great!'
+      @answer = 'Great!'
     elsif @question.include? '?'
-      puts 'Silly question, get dressed and go to work!'
+      @answer = 'Silly question, get dressed and go to work!'
     else
-      puts "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
